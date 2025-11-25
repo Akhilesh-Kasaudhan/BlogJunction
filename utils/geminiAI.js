2cdbec4e3f9c889793b4d166ac11e7ec0e26580d
Akhilesh-Kasaudhan <akhil2003kd@gmail.com>
2025-11-25 14:34:49 +0530

    update generative model version in geminiAI utility

--- utils/geminiAI.js
+++ utils/geminiAI.js
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 export const generateBlogContent = async (prompt) => {
-  const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
   const result = await model.generateContent(prompt);
   const response = result.response;
