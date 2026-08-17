02409e9fda2b6d565fe78a445d98c9ef211afbf4
Akhilesh-Kasaudhan <akhil2003kd@gmail.com>
2026-08-17 13:01:27 +0530

    updated the ai model

--- utils/geminiAI.js
+++ utils/geminiAI.js
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 export const generateBlogContent = async (prompt) => {
   try {
     const model = genAI.getGenerativeModel({
-      model: "gemini-2.0-flash",
+      model: "gemini-3.6-flash",
     });
 
     const result = await model.generateContent(prompt);
